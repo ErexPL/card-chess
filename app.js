2448f2f800f6ad8319a60d6fc421f7618a55b950
ErexPL <zys.dawid@gmail.com>
2025-03-25 14:01:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const rooms = {}; // Store rooms and players
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Server is running...");
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("create-room", () => {
+        const roomId = uuidv4();
+        rooms[roomId] = [socket.id];
+        socket.join(roomId);
+        socket.emit("room-created", roomId);
+        console.log(`Room ${roomId} created by ${socket.id}`);
+    });
+
+    socket.on("join-room", (roomId) => {
+        if (rooms[roomId] && rooms[roomId].length < 2) {
+            rooms[roomId].push(socket.id);
+            socket.join(roomId);
+            io.to(roomId).emit("player-joined", rooms[roomId]);
+            console.log(`${socket.id} joined room ${roomId}`);
+        } else {
+            socket.emit("room-full", roomId);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        for (const [roomId, players] of Object.entries(rooms)) {
+            rooms[roomId] = players.filter((id) => id !== socket.id);
+            if (rooms[roomId].length === 0) {
+                delete rooms[roomId];
+            }
+        }
+        console.log("A user disconnected:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
